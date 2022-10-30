62edf00d32f4dfa2e9d0d835eeaaff0469c12151
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-30 19:18:20 +0100

    Update Order Name function
    Added Update Order Name function into display component

---
+++ BlazorServerDemoApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerDemoApp.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for order name")]
+        [Display(Name = "Name for the Order")]
+        public string OrderName { get; set; }
+    }
+}
